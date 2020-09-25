266c0fae2d0ffb4907c22cf63215eb09cd7f7dcd
a-thread <aiden.threadgoode@gmail.com>
2020-09-25 19:01:23 -0400

    removing space per linter

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
