c68db4ae64415f7992fb639d7ab83e446ee4bf6f
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-09-25 15:39:46 -0400

    post-routes abstracted away attempt

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,27 +1,33 @@
 // const express = require("express");
-const router = require("authenticated-html-routes");
+// const router = require("./authenticated-html-routes");
+module.exports = function(app) {
+  // get method to retrieve past journal entrise in the server
+  app.get("/api/post", (req, res) => {
+    if (!req.user) {
+      res.json({});
+    } else {
+      res.json({ id: result });
+    }
+  });
 
-// get method to retrieve past journal entrise in the server
-router.get("/api/post", (req, res) => {
-  try {
-    console.log(req.body);
-    // need to know what we're getting where
-    res.render("post", console.log(req.body));
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
-  }
-});
+  // post method to send journal entry to the servers
+  // app.post("/api/post", (req, res) => {
+  //   const placeHolderConst = "placeholder";
+  //   try {
+  //     console.log(req.body);
+  //     // need to know what we're logging where
+  //     res.json("index", placeHolderConst);
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.status(500).json(err);
+  //   }
+  // });
 
-// post method to send journal entry to the servers
-router.post("/api/post", (req, res) => {
-  const placeHolderConst = "placeholder";
-  try {
-    console.log(req.body);
-    // need to know what we're logging where
-    res.json("index", placeHolderConst);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
-  }
-});
+  app.post("/api/post", (req, res) => {
+    if (!req.user) {
+      res.json({});
+    } else {
+      res.json({ id: result });
+    }
+  });
+};
