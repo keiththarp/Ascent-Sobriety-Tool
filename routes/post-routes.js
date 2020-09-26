f56771a7b27a50d123da2c6f9f628524ebebadc2
Marc <mlanglois333@icloud.com>
2020-09-26 09:16:54 -0400

    Added routes to retreive all posts by user & single post by ID

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -1,4 +1,7 @@
 // const express = require("express");
+
+const db = require("../models");
+
 // const router = require("./authenticated-html-routes");
 module.exports = function(app) {
   // get method to retrieve past journal entrise in the server
@@ -29,5 +32,29 @@ module.exports = function(app) {
     } else {
       res.json({ id: result });
     }
+  });
+
+  // RETREIVES SINGLE POST BY POST ID
+
+  app.get("/api/post/:id", (req, res) => {
+    db.Post.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbPost => {
+      res.json(dbPost);
+    });
+  });
+
+  //RETRIEVES ALL POSTS BY SINGLE USER ID
+
+  app.get("/api/posts/", (req, res) => {
+    db.Post.findAll({
+      where: {
+        authorId: req.body.id
+      }
+    }).then(dbPost => {
+      res.json(dbPost);
+    });
   });
 };
