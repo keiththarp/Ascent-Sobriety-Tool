4ea706c40ebc508080cab4705b4e6e93939589f9
a-thread <aiden.threadgoode@gmail.com>
2020-09-26 17:41:09 -0400

    adding click functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,11 @@
-// const signIn = $('.signIn')
-// const signUp = $('.signUp')
-// const back = $('.back')
-
-// // these are routes that we will define in our router
-// // user anchor tags with hrefs in the html instead
-// $(".signIn").click(function(){
-//     window.location.replace("signin");
-// });
-// $(".back").click(function(){
-//     window.location.replace(""); //
-// });
-// $(".signUp").click(function(){
-//     window.location.replace("register");
-// });
+// these are routes that we will define in our router
+// user anchor tags with hrefs in the html instead
+$(".signIn").click(() => {
+  window.location.replace("login");
+});
+$(".back").click(() => {
+  window.location.replace("");
+});
+$(".signUp").click(() => {
+  window.location.replace("register");
+});
