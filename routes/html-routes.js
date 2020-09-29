496a13b3d69cdd02af19c826dd66a8bc569167e1
a-thread <aiden.threadgoode@gmail.com>
2020-09-28 21:55:37 -0400

    commenting out routes that have been authenticated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -32,52 +32,56 @@ module.exports = function(app) {
   // see if any mw cred to check first (do that)
   // if pass mw will call next fx
   // then handle response
-  app.get("/daily", (req, res) => {
-    // If the user isn't logged in, send them to the login page
-    if (!req.user) {
-      res.redirect("/login");
-    }
+  // app.get("/daily", (req, res) => {
+  //   // If the user isn't logged in, send them to the login page
+  //   if (!req.user) {
+  //     res.redirect("/login");
+  //   }
 
-    const hbsObj = {
-      user: req.user
-    };
+  //   const hbsObj = {
+  //     user: req.user
+  //   };
 
-    res.render("daily", hbsObj);
-  });
+  //   res.render("daily", hbsObj);
+  // });
 
-  app.get("/counter", (req, res) => {
-    // If the user already has an account send them to the members page
-    // if (!req.user) {
-    //   res.redirect("/login");
-    // }
+  // app.get("/counter", (req, res) => {
+  //   // If the user already has an account send them to the members page
+  //   // if (!req.user) {
+  //   //   res.redirect("/login");
+  //   // }
 
-    res.render("counter");
-  });
+  //   res.render("counter");
+  // });
 
-  app.get("/journal", (req, res) => {
-    // If the user already has an account send them to the members page
-    // if (!req.user) {
-    //   res.redirect("/login");
-    // }
+  // app.get("/journal", (req, res) => {
+  //   // If the user already has an account send them to the members page
+  //   // if (!req.user) {
+  //   //   res.redirect("/login");
+  //   // }
 
-    res.render("journal");
-  });
+  //   const hbsObj = {
+  //     user: req.user
+  //   };
 
-  app.get("/resources", (req, res) => {
-    if (req.user) {
-      res.render("resources");
-    } else {
-      res.redirect("login");
-    }
-  });
+  //   res.render("journal", hbsObj);
+  // });
 
-  // This will need to be an authenticated route at some point
-  app.get("/counter", (req, res) => {
-    // if (req.user) {
-    res.render("counter");
-    // }
-    // res.redirect("start");
-  });
+  // app.get("/resources", (req, res) => {
+  //   if (req.user) {
+  //     res.render("resources");
+  //   } else {
+  //     res.redirect("login");
+  //   }
+  // });
+
+  // // This will need to be an authenticated route at some point
+  // app.get("/counter", (req, res) => {
+  //   // if (req.user) {
+  //   res.render("counter");
+  //   // }
+  //   // res.redirect("start");
+  // });
 
   app.get("/logout", (req, res) => {
     req.logout();
